440e8aa6231d0ae96fe22b6a3d734c20a62f8972
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-19 09:26:06 -0400

    added something

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   get 'scientists/:id/profile', to: 'scientists#show'
   get 'scientists/:id/records', to: 'scientists#records', as: 'scientist_records'
   get 'record/:id', to: 'records#show', as: 'record'
+
   get 'scientists/:id/records/new', to: 'records#new', as: 'new_record'
+  
   post 'scientists/:id/records/new', to: 'scientists#create_record'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
